679c483cef1c2c02373afd0e5cad9f8dc2f62d31
KrisDLuna <krisdluna@hotmail.com>
2018-01-19 05:19:06 +0800

    Ayana bot style
    Inspired by Ayana bot's =avatar function, this embeds the image and hides the URL to give a cleaner output.

--- avatar/avatar.py
+++ avatar/avatar.py
@@ -1,11 +1,12 @@
 import discord
 from discord.ext import commands
+from random import choice, randint
 import re
 
 
 def process_avatar(url):
     if ".gif" in url:
-        new_url = re.sub("\?size\=\d+$", "", url)
+        new_url = url + "&f=.gif"
         return new_url
     else:
         new_url = url.replace('.webp', '.png')
@@ -27,8 +28,23 @@ class Avatar:
             user = author
 
         u = user.avatar_url
+        colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+        colour = int(colour, 16)
+        randnum = randint(1, 10)
+        empty = u"\u2063"
+        emptyrand = empty * randnum
         url = process_avatar(u)
-        await self.bot.say("{}'s Avatar URL : {}".format(user.name, url))
+        if url:
+            data = discord.Embed(title="Direct Link", url=url, colour=discord.Colour(value=colour))
+            data.set_image(url=url)
+        if user:
+            data.set_author(name=user.name)
+        else:
+            data.set_author(name=author.name)
+        try:
+            await self.bot.say(emptyrand, embed=data)
+        except:
+            await self.bot.say("Try giving me permission to embed links.")
 
 
 def setup(bot):
